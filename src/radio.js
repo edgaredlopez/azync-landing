b23e7050f11ef6c7a9cd219f65746ca6e0f812cf
Edgar López <edgared.dev@gmail.com>
2022-12-28 23:34:42 -0600

    Coreccion de desorden que genero la herramienta ghpages

--- src/radio.js
+++ src/radio.js
@@ -5,21 +5,14 @@ const cover = null || document.getElementById("cover");
 const title = null || document.getElementById("title");
 const artist = null || document.getElementById("artist");
 const album = null || document.getElementById("album");
-const EtiquetaAudioEdgar= null || document.getElementById("EtiquetaAudio");
+const EtiquetaAudioEdgar = null || document.getElementById("EtiquetaAudio");
 const oyentes = null || document.getElementById("oyentes");
 
-//Saber si el navegador es firefox o chrome
-
-
+//Verificar si el navegador es firefox
 if (!navigator.userAgent.includes('Firefox')) 
 {
     EtiquetaAudioEdgar.style.backgroundColor = 'transparent';
-    console.log('No es firefox');
 }
-else{
-    console.log('Es firefox');
-}
-
 
 const opciones = {
     method: "GET",
@@ -44,60 +37,69 @@ let PermisoActulizar = false; // Permiso para actualizar la canción
 let RadioPausado = true; // Si la radio esta pausada
 
 
-async function Logica() {
+async function Logica() 
+{
     if (PermisoActulizar == true) 
     {
-        try {
+        try
+        {
             const DatosRadio = await ObtenerRadio();
 
             //Guardar los datos de la api en variables
+            let IDCancionAPI = DatosRadio.now_playing.song.id;
             let Cover = DatosRadio.now_playing.song.art;
             let Title = DatosRadio.now_playing.song.title;
             let Artist = DatosRadio.now_playing.song.artist;
             let Album = DatosRadio.now_playing.song.album;
             let direccionAudio = DatosRadio.station.listen_url;
             let Oyentes = DatosRadio.listeners.current;
-            let IDCancionAPI = DatosRadio.now_playing.song.id;
 
             let remaining = DatosRadio.now_playing.remaining;
-            let duration = DatosRadio.now_playing.duration;
 
             if (remaining > 30) 
             {
                 intervaloActualizacion = 10000;
-            } else if (remaining <= 30) {
+            } else if (remaining <= 30)
+            {
                 intervaloActualizacion = 5000;
+            }
+            //Verificar si es la primera cancion que se reproduce la radio actual
+            if(IDCancionActual==0)
+            {
+                EtiquetaAudioEdgar.src = null;
+                EtiquetaAudioEdgar.src = direccionAudio + "?rand=" + Math.random();
+                EtiquetaAudioEdgar.load();
+                EtiquetaAudioEdgar.play();
             }
 
             //Verificar si la cancion actual es diferente a la cancion que esta sonando en la radio
             if (IDCancionActual != IDCancionAPI) 
             {
-                //Asignar los datos a las etiquetas de HTML
                 cover.setAttribute("src", Cover);
                 title.textContent = Title;
                 artist.textContent = Artist;
                 album.textContent = Album;
 
-                EtiquetaAudioEdgar.src = null;
-                EtiquetaAudioEdgar.src = direccionAudio + "?rand=" + Math.random();
-                if (IDCancionActual != 0) //Verificamos que no sea la primera vez que se ejecuta el codigo
+                //Evaluar si el tiempo reproducido es mayor que 10 minutos para reiniciar el progreso de la barra
+                if(EtiquetaAudioEdgar.currentTime > 600)
                 {
+                    EtiquetaAudioEdgar.src = null;
+                    EtiquetaAudioEdgar.src = direccionAudio + "?rand=" + Math.random();
+                    EtiquetaAudioEdgar.load();
                     EtiquetaAudioEdgar.play(); //aqui se reproduce la nueva cancion
                 }
-                IDCancionActual = IDCancionAPI;
             }
-
+            IDCancionActual = IDCancionAPI;
             oyentes.textContent = Oyentes; //Actualizar los datos de los oyentes
 
             //Consultar la API de la radio cada cierto tiempo dependiendo del tiempo que le quede a la cancion
             setTimeout(() =>
             {
                 Logica();
-                console.log("Se actualizo la radio " + intervaloActualizacion + "  Restante " + remaining);
             }, intervaloActualizacion);
 
         } catch (error) {
-            console.log("Alguna de las peticiones devolvio un REJECT, se capturo y es : " + error);
+            console.log("La petición generó un REJECT: " + error);
         }
     }
 }
@@ -108,7 +110,6 @@ Logica();
 //Detectar cuando el usuario le da pausa a la radio
 EtiquetaAudioEdgar.addEventListener("pause", ()=> 
 {
-    console.log("Se pauso la radio");
     EtiquetaAudioEdgar.src = null; //Mandar null al src para que no se siga reproduciendo la cancion
     EtiquetaAudioEdgar.src = 'https://coderadio-relay-nyc.freecodecamp.org/radio/8010/low.mp3' + "?rand=" + Math.random();
     EtiquetaAudioEdgar.currentTime = 0;
@@ -119,16 +120,12 @@ EtiquetaAudioEdgar.addEventListener("pause", ()=>
 //Detectar cuando el usuario le da play a la radio
 EtiquetaAudioEdgar.addEventListener("play", ()=> 
 {
-    console.log("Se detecto play");
     if (RadioPausado == true) 
     {
-        console.log("Se reproducio la radio");
-        EtiquetaAudioEdgar.play();
         PermisoActulizar = true;
         RadioPausado = false;
         Logica();
     }
-    
 });
 
 
